c7d23e2ce17ff02832b2f800787fc4d18b5ff047
t-siri <t-siri@github.com>
2022-05-16 22:23:47 +1000

    mobile responsive

--- _site/assets/js/nav-toggle.js
+++ _site/assets/js/nav-toggle.js
@@ -1,24 +1,3 @@
-// function toggleContact() {
-// 	var contact = document.getElementById('contact-content');
-// 	contact.classList.toggle('contact-content__visible');
-// 	contact.classList.toggle('contact-content__hidden');
-// }
-
-// function toggleContactX() {
-// 	var contact = document.getElementById('contact-content');
-// 	var close = document.getElementById('close-nav');
-// 	var formClose = document.getElementById('form-close');
-// 	close.addEventListener('click', function() {
-// 		contact.classList.toggle('contact-content__visible');
-// 		contact.classList.toggle('contact-content__hidden');
-// 	});
-// 	formClose.addEventListener('click', function() {
-// 		contact.classList.toggle('contact-content__visible');
-// 		contact.classList.toggle('contact-content__hidden');
-// 	});
-// }
-// toggleContactX()
-
 function toggleMobNav() {
   var burger = document.getElementById("menu-link");
   var navicon = document.getElementById("nav-icon1");
